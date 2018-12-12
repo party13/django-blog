f5f5269ef0e9682a71f9e79588832f766a6c1957
party13 <partyzzan.real@gmail.com>
2018-12-12 17:34:43 +0200

    django tutorial project - Blog with posts 12Dec2018. Ready.Stable.Initial commit

---
+++ blog/utils.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import *
+
+class ObjectDetailMixin:
+    model = None
+    template = None
+    button_name = ''
+
+    def get(self, request, slug):
+        #post = Post.objects.get(slug__iexact=slug)
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        return render(request, self.template,
+                      context={self.model.__name__.lower(): obj,
+                               'button_name': self.button_name,
+                               'admin_object': obj,
+                               'detail': True })
+
+
+class ObjectCreateMixin:
+    model = None
+    model_form = None
+    template = None
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+
+        if bound_form.is_valid():
+            print ('form is valid')
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+
+        print ('form not valid')
+        return render(request, self.template, context={'form': bound_form})
+
+
+class ObjectUpdateMixin:
+    model = None
+    model_form = None
+    template = None
+    button_name = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(instance=obj)
+        return render(request, self.template, context= {'form': bound_form, self.model.__name__.lower(): obj, 'button_name': self.button_name})
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(request.POST, instance=obj)
+
+        if bound_form.is_valid():
+            print('successfully updated')
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        print ('update failed!')
+        return render (request, self.template, context = {'form': bound_form, self.model.__name__.lower(): obj, 'button_name': self.button_name})
+
+
+class ObjectDeleteMixin:
+    model = None
+    template = None
+    redirect_url = None
+    button_name = None
+    #obj = self.model.objects.get(slug__iexact=slug)
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact = slug)
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'button_name': self.button_name})
+
+    def post(self,  request, slug):
+        obj = self.model.objects.get(slug__iexact = slug)
+        obj.delete()
+        return redirect(reverse(self.redirect_url))
